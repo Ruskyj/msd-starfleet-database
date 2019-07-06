ca94bd70c1304c247fbdb667a0ea50004b91aa1a
Ladislav Jochman <l.jochman@zone42.sk>
2019-07-06 15:15:37 +0200

    Proper error handling & status codes

--- server-local.js
+++ server-local.js
@@ -1,8 +1,5 @@
 require('dotenv').config()
 const app = require('./lib/app')
-const logger = require('./middleware/logger')
-
 const PORT = process.env.PORT || 7000
 
-app.use(logger)
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}...`))
